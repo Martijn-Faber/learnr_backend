2f7aa2d68bc8131cddf37466a2f260683ba71453
Martijn Faber <martijn@fabtronics.nl>
2021-06-26 21:04:15 +0200

    feat(config): add types for config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,12 @@ const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 8000;
 const SERVER_PREFIX = process.env.SERVER_PREFIX;
 
+interface Server {
+  hostname: string;
+  port: number;
+  prefix: string;
+}
+
 const SERVER = {
   hostname: SERVER_HOSTNAME,
   port: SERVER_PORT,
@@ -13,9 +19,18 @@ const SERVER = {
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+interface Jwt {
+  secret: string;
+}
+
 const JWT = {
   secret: JWT_SECRET,
 };
+
+interface Config {
+  server: Server;
+  jwt: Jwt;
+}
 
 const config = {
   server: SERVER,
