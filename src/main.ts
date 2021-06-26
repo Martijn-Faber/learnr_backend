5ba1ca892456d5e1cbc84461c86ab51592638190
Martijn Faber <martijn@fabtronics.nl>
2021-06-26 17:00:30 +0200

    feat(logger): add logger and fix type

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
 import config from '@/config/config';
 import { HttpExceptionFilter } from '@/filters/http-exception.filter';
+import { Logger } from '@/utils/logger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('server');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger,
+  });
 
   app.enableCors();
   app.setGlobalPrefix(config.server.prefix);
