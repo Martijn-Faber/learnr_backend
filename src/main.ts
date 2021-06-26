9b87e7906808897406c0075ae22108270acbb86f
Martijn Faber <martijn@fabtronics.nl>
2021-06-26 18:32:16 +0200

    feat(filters): add response interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
 import config from '@/config/config';
 import { HttpExceptionFilter } from '@/filters/http-exception.filter';
+import { ResponseInterceptor } from '@/filters/response.interceptor';
 import { Logger } from '@/utils/logger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
@@ -15,6 +16,7 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix(config.server.prefix);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(config.server.port);
 }
 bootstrap();
