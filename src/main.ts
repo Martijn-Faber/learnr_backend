97c879104ff64fa65d2ff34103981cff23c19c15
Martijn Faber <martijn@fabtronics.nl>
2021-06-26 21:04:51 +0200

    feat(swagger): add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from '@/filters/http-exception.filter';
 import { ResponseInterceptor } from '@/filters/response.interceptor';
 import { Logger } from '@/utils/logger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('server');
@@ -17,6 +18,15 @@ async function bootstrap() {
   app.setGlobalPrefix(config.server.prefix);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Learnr docs')
+    .setDescription('A place to study. ')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(config.server.port);
 }
 bootstrap();
